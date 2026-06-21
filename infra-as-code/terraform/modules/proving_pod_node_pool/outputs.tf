b5759ec5925bf932c63b2b684715accc90d9438c
Kunal Limaye <kunallimaye@gmail.com>
2026-06-21 11:46:18 +1000

    fix: use safe try(...) indexing in proving_pod_node_pool outputs to prevent Invalid index errors (#345)
    TAG=agy
    CONV=6cb53cc9-4468-4d1f-a022-54339b191328

--- infra-as-code/terraform/modules/proving_pod_node_pool/outputs.tf
+++ infra-as-code/terraform/modules/proving_pod_node_pool/outputs.tf
@@ -3,10 +3,10 @@
 
 output "leaf_fleet_id" {
   description = "Identifier of the provisioned Leaf Worker fleet (Node Pool or MIG)"
-  value       = var.orchestration_engine == "gke" ? google_container_node_pool.leaf_worker_pool[0].id : google_compute_region_instance_group_manager.leaf_mig_fleet[0].id
+  value       = try(google_container_node_pool.leaf_worker_pool[0].id, try(google_compute_region_instance_group_manager.leaf_mig_fleet[0].id, ""))
 }
 
 output "aggregator_fleet_id" {
   description = "Identifier of the provisioned Tree Aggregator fleet"
-  value       = var.orchestration_engine == "gke" ? google_container_node_pool.aggregator_pool[0].id : ""
+  value       = try(google_container_node_pool.aggregator_pool[0].id, "")
 }
